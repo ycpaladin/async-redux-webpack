c97545b0ec9b1eceee0b30d6d4b9864d897dac2c
kevin.chen <ycpaladin@126.com>
2016-08-10 10:19:53 +0800

    加入路由

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
-// var ExtractTextPlugin = require("extract-text-webpack-plugin")
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -11,26 +12,28 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js'
     },
+    devServer: {
+        // historyApiFallback: true,
+        hot: true,
+        inline: true,
+        // progress: true,
+        port: 9000
+    },
+    devtool: 'sourcemap',
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                // include: path.join(__dirname, 'public/app'),
-                // loaders: ['babel'],
-                query: {
-                    presets: ['react', 'es2015']
-                }
+                loader: 'babel?presets[]=es2015&presets[]=react&plugins[]=transform-runtime&plugins[]=syntax-async-functions&plugins[]=syntax-decorators'
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("style", "css!sass")
             },
             {
                 test: /\.css$/,
-                loader: [
-                    {
-                        test: /\.css$/,
-                        loader: 'style!css'
-                    }
-                ]
+                loader: ExtractTextPlugin.extract("style", "css")
             },
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
@@ -41,7 +44,15 @@ module.exports = {
         // new webpack.DefinePlugin({
         //     __DEBUG__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
         // }),
-        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+        new ExtractTextPlugin('[name].css'),
+        new HtmlwebpackPlugin({
+            title: 'Hello app',
+            // filename: 'index.html',
+            template:'index.html',
+            chunks: ['vendors', 'app'],
+            inject: 'body'
+        })
         // new webpack.optimize.UglifyJsPlugin({
         //     compress: {
         //         warnings: false
